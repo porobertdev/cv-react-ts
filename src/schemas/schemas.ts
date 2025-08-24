fa111bc56c0226894cc9ffddf8d302af4f547e7f
Robert P <porobertdev@gmail.com>
2025-08-24 09:00:42 +0300

    feat(schemas): add zod types

---
+++ src/schemas/schemas.ts
@@ -0,0 +1,97 @@
+import { z } from 'zod';
+
+// enums
+export const PlatformEnum = z.enum([
+  'Twitter',
+  'LinkedIn',
+  'GitHub',
+  'Facebook',
+  'Instagram',
+  'Website',
+]);
+export const SkillLevelEnum = z.enum(['Beginner', 'Intermediate', 'Advanced', 'Expert']);
+export const SkillTypeEnum = z.enum(['Technical', 'Soft', 'Language', 'Other']);
+
+export const AboutSchema = z.object({
+  profilePic: z.string().url().optional(),
+  fName: z.string().min(1, 'First name is required').max(50),
+  lName: z.string().min(1, 'Last name is required').max(50),
+  city: z.string().min(1).max(50).optional(),
+  country: z.string().min(1).max(50).optional(),
+  intro: z.string().min(1).max(300).optional(),
+});
+
+export const ContactSchema = z.object({
+  email: z.string().email().optional(),
+  phone: z
+    .string()
+    .regex(/^\+?[1-9]\d{1,14}$/)
+    .optional(),
+  socialLinks: z
+    .array(
+      z.object({
+        platform: PlatformEnum.optional(),
+        url: z.string().url('invalid URL'),
+        username: z.string().min(1, 'Username is required').max(50).optional(),
+      }),
+    )
+    .optional(),
+});
+
+export const ExperienceSchema = z.object({
+  id: z.string().uuid().optional(),
+  jobTitle: z.string().min(1, 'Job title is required').max(100),
+  company: z.string().min(1, 'Company name is required').max(100),
+  location: z.string().min(1).max(100).optional(),
+  startDate: z.date().min(new Date('1900-01-01'), 'Start date is required'),
+  endDate: z.string().optional(),
+  isCurrent: z.boolean().optional(),
+  description: z.string().max(1000).optional(),
+});
+
+export const EducationSchema = z.object({
+  id: z.string().uuid().optional(),
+  institution: z.string().min(1, 'Institution name is required').max(100),
+  degree: z.string().min(1, 'Degree is required').max(100),
+  fieldOfStudy: z.string().min(1, 'Field of study is required').max(100),
+  startDate: z.date().min(new Date('1900-01-01'), 'Start date is required'),
+  endDate: z.string().optional(),
+  isCurrent: z.boolean().optional(),
+  description: z.string().max(1000).optional(),
+});
+
+export const SkillSchema = z.object({
+  id: z.string().uuid().optional(),
+  name: z.string().min(1, 'Skill name is required').max(50),
+  proficiency: SkillLevelEnum.optional(),
+  type: SkillTypeEnum.optional(),
+});
+
+export const ProjectSchema = z.object({
+  id: z.string().uuid().optional(),
+  name: z.string().min(1, 'Project name is required').max(100),
+  description: z.string().max(1000).optional(),
+  links: z.object({
+    live: z.string().url().optional(),
+    git: z.string().url().optional(),
+  }),
+  technologies: z.array(z.string().min(1).max(50)).optional(),
+});
+
+export const ResumeSchema = z.object({
+  about: AboutSchema,
+  contact: ContactSchema.optional(),
+  experiences: z.array(ExperienceSchema).optional(),
+  education: z.array(EducationSchema).optional(),
+  skills: z.array(SkillSchema).optional(),
+  projects: z.array(ProjectSchema).optional(),
+});
+
+export type About = z.infer<typeof AboutSchema>;
+export type Contact = z.infer<typeof ContactSchema>;
+export type Experience = z.infer<typeof ExperienceSchema>;
+export type Education = z.infer<typeof EducationSchema>;
+export type Skill = z.infer<typeof SkillSchema>;
+export type Project = z.infer<typeof ProjectSchema>;
+export type Resume = z.infer<typeof ResumeSchema>;
+export type Platform = z.infer<typeof PlatformEnum>;
